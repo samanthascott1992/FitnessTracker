f260474d4d0a6f76808c4817b79c72c3c318d1d8
Samantha Scott <Samanthascott1992@gmail.com>
2020-05-11 19:33:46 -0600

    fixing server.js

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ mongoose.connect("mongodb://localhost/workout", {
 
 // routes
 
-app.use(require(".routes/api.js"));
+app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}`);
+    console.log(`App running on port ${PORT}!`);
 });
