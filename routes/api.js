1835f95bc3a5f681900e6ad6150128721f7c3c5a
Samantha Scott <Samanthascott1992@gmail.com>
2020-05-12 07:57:30 -0600

    starting on api routes

---
+++ routes/api.js
@@ -0,0 +1,15 @@
+app.get("/api/workouts", (req, res) => {
+
+});
+
+app.put("/api/workouts", (req, res) => {
+
+});
+
+app.post("/api/workouts", (req, res) => {
+
+});
+
+app.get("/api/workouts/range", (req, res) => {
+
+});
