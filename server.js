2627fc6adf43b68a11688762d9927960b2698217
Samantha Scott <Samanthascott1992@gmail.com>
2020-05-14 17:33:27 -0600

    updated server for Heroku deployment

--- server.js
+++ server.js
@@ -26,3 +26,9 @@ app.use(require("./routes/view.js"));
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
+
+// HEROKU DEPLOYMENT
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
