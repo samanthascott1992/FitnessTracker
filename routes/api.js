13d6115eb23e317b91ba66e48bf67f0ee78825f6
Samantha Scott <Samanthascott1992@gmail.com>
2020-05-14 17:34:05 -0600

    updated api.js

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,49 @@
-
-
-app.get("/api/workouts", (req, res) => {
-
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then(dbWorkout => { res.json(dbWorkout) })
+        .catch(err => { res.json(err) })
+})
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        { new: true, runValidators: true }
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
-
-app.put("/api/workouts", (req, res) => {
-
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
-
-app.post("/api/workouts", (req, res) => {
-
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({}).limit(7)
+  .then(dbWorkouts => {
+    console.log(dbWorkouts)
+    res.json(dbWorkouts);
+  })
+  .catch(err => {
+    res.json(err);
+  });
 });
-
-app.get("/api/workouts/range", (req, res) => {
-
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+  .then(() => {
+    res.json(true);
+  })
+  .catch(err => {
+    res.json(err);
+  });
 });
+module.exports = router;
